a524a960c2ab5c05d3cb552c6d2be38c76a19bb0
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2023-02-04 16:34:09 -0300

    Dinamização do template, finalização do controle de rotas

---
+++ src/popeye/organisms/Header.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import NavBar from "../molecules/Navbar";
+
+const HeaderSection = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #F4A460;
+  color: black;
+  min-height: 10vh;
+  padding: 10px;
+`;
+
+
+const Header = (props: any) => {
+
+  return (
+    <HeaderSection>
+      <NavBar />
+    </HeaderSection>
+  );
+};
+
+export default Header;
